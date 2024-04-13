260ca8e813dfc233ff0b0e7d5fcb17942d730720
alistairwalsh <alistairwalsh@me.com>
2024-04-13 23:51:44 +1000

    button

--- app/voice_app.py
+++ app/voice_app.py
@@ -1,14 +1,7 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-from crewai import Agent, Task, Crew, Process
 import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
-
-st.image('images/crew_logo.png')
 
 stt_button = Button(label="Speak", width=100)
 
